24edfc6b81b45d077c2ecff6f463e406ad83d1d7
lsclgr <calegariluisa@gmail.com>
2021-02-25 19:56:52 -0300

    test user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -19,7 +19,7 @@ class UserController{
             name, email
         })
         await usersRepository.save(user)
-        return response.json(user);
+        return response.status(201).json(user);
     }
 }
 
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,15 @@
-import {createConnection} from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+export default async (): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  //verifica se e teste pra saber qual banco usar
+  return createConnection(
+    Object.assign(defaultOptions, {
+      database:
+        process.env.NODE_ENV === "test"
+          ? "./src/database/database.test.sqlite"
+          : defaultOptions.database,
+    })
+  );
+};
